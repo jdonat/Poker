c11d15bab5e0fd37b5bfc88dacc06b297225e5f8
John Doe <j.donatmagnin@it-students.fr>
2024-03-10 18:42:36 +0100

    Server&Client

---
+++ com/poker/src/client/SSLSocketClient.java
@@ -0,0 +1,34 @@
+package com.poker.src.client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.OutputStream;
+import java.io.BufferedOutputStream;
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+public class SSLSocketClient {
+
+    static void startClient(String host, int port) throws IOException {
+
+        SocketFactory factory = SSLSocketFactory.getDefault();
+        try (SSLSocket socket = (SSLSocket) factory.createSocket(host, port)) {
+            
+            socket.setEnabledCipherSuites(new String[] { "TLS_AES_128_GCM_SHA256" });
+            socket.setEnabledProtocols(new String[] { "TLSv1.3" });
+            
+            String message = "Hello World Message";
+            System.out.println("sending message: " + message);
+            OutputStream os = new BufferedOutputStream(socket.getOutputStream());
+            os.write(message.getBytes());
+            os.flush();
+            
+            InputStream is = new BufferedInputStream(socket.getInputStream());
+            byte[] data = new byte[2048];
+            int len = is.read(data);
+            System.out.printf("client received %d bytes: %s%n", len, new String(data, 0, len));
+        }
+    }
+}
